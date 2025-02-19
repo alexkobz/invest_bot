ef4c003ed9660803959ad18fdc68c2ca73562d09
alexkobz <alexanderkobzarrr@gmail.com>
2025-02-19 12:46:25 +0300

    FIX girbo parse_fundamentals

--- api_model_python/GirboData.py
+++ api_model_python/GirboData.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import os
-from io import BytesIO
-
+from collections import defaultdict
+import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -14,7 +14,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text as sa_text
 from time import sleep
-from typing import List, Dict, Union
+from typing import List, Dict
 
 from api_model_python.plugins.path import get_project_root, Path
 
@@ -196,106 +196,73 @@ class GirboData:
             logger.info(f"{card} success")
             sleep(1)
 
+    # TODO refactor function
     def parse_fundamentals(self):
 
         import zipfile
-        from openpyxl import load_workbook
-        from dataclasses import dataclass
-        from openpyxl.workbook import Workbook
-        from openpyxl.worksheet.worksheet import Worksheet
 
-        @dataclass
-        class FundamentalsData:
-            sheet: Worksheet
-            cells_range: str
-            rows: List[int]
+        balance_columns = ['1110', '1120', '1130', '1140', '1150', '1160', '1170', '1180', '1190', '1100', '1210',
+                           '1220', '1230', '1240', '1250', '1260', '1200', '1600', '1310', '1320', '1340', '1350',
+                           '1360', '1370', '1300', '1410', '1420', '1430', '1450', '1400', '1510', '1520', '1530',
+                           '1540', '1550', '1500', '1700']
+        financial_results_columns = ['2110', '2120', '2100', '2210', '2220', '2200', '2310', '2320', '2330', '2340',
+                                     '2350', '2300', '2410', '2411', '2412', '2460', '2400', '2510', '2520', '2530',
+                                     '2500', '2900', '2910', '2421', '2430', '2450']
+        additional_financial_results_columns = ['2410', '2421', '2430', '2450']
+        target_usage_columns = ['6100', '6210', '6215', '6220', '6230', '6240', '6250', '6200', '6310', '6311', '6312',
+                                '6313', '6320', '6321', '6322', '6323', '6324', '6325', '6326', '6330', '6350', '6300',
+                                '6400']
+        capital_movement_columns = ['3100', '3210', '3211', '3212', '3213', '3214', '3215', '3216', '3220', '3221',
+                                    '3222', '3223', '3224', '3225', '3226', '3227', '3230', '3240', '3200', '3310',
+                                    '3311', '3312', '3313', '3314', '3315', '3316', '3320', '3321', '3322', '3323',
+                                    '3324', '3325', '3326', '3327', '3330', '3340', '3300']
+        capital_correctness_columns = ['3400', '3410', '3420', '3500', '3401', '3411', '3421', '3501', '3402', '3412',
+                                       '3422', '3502']
+        clean_assets_columns = ['3600']
+        cash_flow_columns = ['4110', '4111', '4112', '4113', '4119', '4120', '4121', '4122', '4123', '4124', '4129',
+                             '4100', '4210', '4211', '4212', '4213', '4214', '4219', '4220', '4221', '4222', '4223',
+                             '4224', '4229', '4200', '4310', '4311', '4312', '4313', '4314', '4319', '4320', '4321',
+                             '4322', '4323', '4329', '4300', '4400', '4450', '4500', '4490']
 
-        class FundamentalsParser:
+        balance_matrix = np.array(np.meshgrid(
+            balance_columns,
+            ['4', '5', '6'])).T.reshape(-1, 2)
+        financial_results_matrix = np.array(np.meshgrid(
+            financial_results_columns,
+            ['4', '5'])).T.reshape(-1, 2)
+        additional_financial_results_matrix = np.array(np.meshgrid(
+            additional_financial_results_columns,
+            ['4', '5'])).T.reshape(-1, 2)
+        target_usage_matrix = np.array(np.meshgrid(
+            target_usage_columns,
+            ['4', '5'])).T.reshape(-1, 2)
+        capital_movement_matrix = np.array(np.meshgrid(
+            capital_movement_columns,
+            ['8'])).T.reshape(-1, 2)
+        capital_correctness_matrix = np.array(np.meshgrid(
+            capital_correctness_columns,
+            ['3', '6'])).T.reshape(-1, 2)
+        cash_flow_matrix = np.array(np.meshgrid(
+            cash_flow_columns,
+            ['3', '4'])).T.reshape(-1, 2)
 
-            def __init__(self, wb):
-                self.workbook: Workbook = wb
-                self.main = FundamentalsData(
-                    sheet=self._get_worksheet("Сведения об организации"),
-                    cells_range="A10:M10",
-                    rows=[0, 12])
-                self.non_current_assets = FundamentalsData(
-                    sheet=self._get_worksheet("Бухгалтерский баланс"),
-                    cells_range="N9:AG19",
-                    rows=[0, 4, 11, 19])
-                self.current_assets = FundamentalsData(
-                    sheet=self._get_worksheet("Бухгалтерский баланс"),
-                    cells_range="N20:AG29",
-                    rows=[0, 4, 11, 19])
-                self.capital_reserves = FundamentalsData(
-                    sheet=self._get_worksheet("Бухгалтерский баланс"),
-                    cells_range="N30:AG37",
-                    rows=[0, 4, 11, 19])
-                self.longterm_liabilities = FundamentalsData(
-                    sheet=self._get_worksheet("Бухгалтерский баланс"),
-                    cells_range="N38:AG43",
-                    rows=[0, 4, 11, 19])
-                self.shortterm_liabilities = FundamentalsData(
-                    sheet=self._get_worksheet("Бухгалтерский баланс"),
-                    cells_range="N44:AG50",
-                    rows=[0, 4, 11, 19])
-                self.financial_results = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет о финансовых результатах"),
-                    cells_range="Q7:AD30",
-                    rows=[0, 6, 13])
-                self.financial_results_addition = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет о финансовых результатах"),
-                    cells_range="Q38:AD42",
-                    rows=[0, 6, 13])
-                self.target_usage_cash = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет о целевом использовании д"),
-                    cells_range="P7:AC31",
-                    rows=[0, 6, 13])
-                self.capital_movement = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет об изменениях капитала"),
-                    cells_range="F8:AF47",
-                    rows=[0, 26])
-                self.capital_correctness = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет об изменениях капитала"),
-                    cells_range="J52:AE67",
-                    rows=[0, 21])
-                self.clean_assets = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет об изменениях капитала"),
-                    cells_range="I69:L70",
-                    rows=[0, 3])
-                self.cash_flow = FundamentalsData(
-                    sheet=self._get_worksheet("Отчет о движении денежных средс"),
-                    cells_range="Q8:AD50",
-                    rows=[0, 6, 13])
-
-            def _get_worksheet(self, sheet_name):
-                """Safely get a worksheet by name from the workbook."""
-                try:
-                    return self.workbook[sheet_name]
-                except KeyError:
-                    return None
-
-            @staticmethod
-            def parse_sheet(fundamentals_data: FundamentalsData):
-                values = [[cell.value for cell in row] for row in fundamentals_data.sheet[fundamentals_data.cells_range]]
-                df = pd.DataFrame(values).T
-                df = df.loc[fundamentals_data.rows]
-                df.columns = df.loc[0]
-                df = df.drop(df.index[0]).reset_index(drop=True)
-                try:
-                    del df[None]
-                except KeyError:
-                    pass
-                for col in df.select_dtypes(include=['object', 'string']):
-                    df[col] = (
-                        df[col]
-                        .str.replace('(-)2', '', regex=False)
-                        .str.replace(r'[()\sХ-]', '', regex=True))
-                    df[col] = pd.to_numeric(
-                        df[col],
-                        downcast='float',
-                        errors='ignore')
-                df.index = list(map(lambda x: year - x, df.index))
-                return df
+        @staticmethod
+        def _parse_sheet(df: pd.DataFrame, matrix) -> pd.DataFrame:
+            try:
+                res = defaultdict(list)
+                for code, column in matrix:
+                    try:
+                        row = df[df == code].any(axis=1)
+                        row = row.index[row][0]
+                        col = df[df == column].any(axis=0)
+                        col = col.index[col][0]
+                        res[code].append(df.loc[row, col])
+                    except:
+                        res[code].append(None)
+                return pd.DataFrame(res)
+            except Exception as e:
+                logger.exception(e)
+                return pd.DataFrame()
 
         table_name: str = 'api_girbo_fundamentals'
         self.engine.execute(
@@ -310,45 +277,110 @@ class GirboData:
                 try:
                     with zipfile.ZipFile(filename, 'r') as zip_ref:
                         with zip_ref.open(zip_ref.namelist()[0]) as file_stream:
-                            workbook: Workbook = load_workbook(filename=BytesIO(file_stream.read()))
-                    fundamentals_parser = FundamentalsParser(workbook)
-                    non_current_assets: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.non_current_assets)
-                    current_assets: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.current_assets)
-                    capital_reserves: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.capital_reserves)
-                    longterm_liabilities: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.longterm_liabilities)
-                    shortterm_liabilities: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.shortterm_liabilities)
-                    financial_results: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.financial_results)
-                    financial_results_addition: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.financial_results_addition)
-                    target_usage_cash: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.target_usage_cash)
-                    capital_movement: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.capital_movement)
-                    capital_correctness: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.capital_correctness)
-                    clean_assets: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.clean_assets)
-                    cash_flow: pd.DataFrame = fundamentals_parser.parse_sheet(
-                        fundamentals_parser.cash_flow)
-                    inn_chunk: pd.DataFrame = pd.concat(
-                        [non_current_assets, current_assets, capital_reserves, longterm_liabilities, shortterm_liabilities,
-                         financial_results, financial_results_addition, target_usage_cash,
+                            try:
+                                balance_df = pd.read_excel(file_stream, sheet_name='Бухгалтерский баланс')
+                            except:
+                                logger.warning(f'No sheet "Бухгалтерский баланс" for inn {inn}')
+                                balance_df = pd.DataFrame()
+                            try:
+                                finance_df = pd.read_excel(file_stream, sheet_name='Отчет о финансовых результатах')
+                            except:
+                                logger.warning(f'No sheet "Отчет о финансовых результатах" for inn {inn}')
+                                finance_df = pd.DataFrame()
+                            try:
+                                target_usage_df = pd.read_excel(file_stream, sheet_name='Отчет о целевом использовании д')
+                            except:
+                                logger.warning(f'No sheet "Отчет о целевом использовании д" for inn {inn}')
+                                target_usage_df = pd.DataFrame()
+                            try:
+                                capital_df = pd.read_excel(file_stream, sheet_name='Отчет об изменениях капитала')
+                            except:
+                                logger.warning(f'No sheet "Отчет об изменениях капитала" for inn {inn}')
+                                capital_df = pd.DataFrame()
+                            try:
+                                cash_flow_df = pd.read_excel(file_stream, sheet_name='Отчет о движении денежных средс')
+                            except:
+                                logger.warning(f'No sheet "Отчет о движении денежных средс" for inn {inn}')
+                                cash_flow_df = pd.DataFrame()
+                    balance = _parse_sheet(balance_df, balance_matrix)
+
+                    codes = finance_df[finance_df.isin(['Код строки', 'Код строк'])].stack().index.tolist()
+                    rows_split = [row[0] for row in codes]
+                    financial_results_df = pd.DataFrame()
+                    additional_financial_results = pd.DataFrame()
+                    if len(rows_split) == 1:
+                        financial_results_df = finance_df.copy()
+                    elif len(rows_split) == 2:
+                        financial_results_df = finance_df[
+                            (finance_df.index >= rows_split[0]) & (finance_df.index < rows_split[1])]
+                        additional_financial_results_df = finance_df[(finance_df.index >= rows_split[1])]
+                        additional_financial_results = _parse_sheet(additional_financial_results_df,
+                                                                    additional_financial_results_matrix)
+                    financial_results = _parse_sheet(financial_results_df, financial_results_matrix)
+
+                    target_usage = _parse_sheet(target_usage_df, target_usage_matrix)
+                    capital_movement = pd.DataFrame()
+                    capital_correctness = pd.DataFrame()
+                    clean_assets = pd.DataFrame()
+                    if not capital_df.empty:
+                        codes = capital_df[capital_df.isin(['Код строки', 'Код строк'])].stack().index.tolist()
+                        rows_split = [row[0] for row in codes]
+                        capital_movement_df = capital_df[
+                            (capital_df.index >= rows_split[0]) & (capital_df.index < rows_split[1])]
+                        capital_correctness_df = capital_df[
+                            (capital_df.index >= rows_split[1]) & (capital_df.index < rows_split[2])]
+                        clean_assets_df = capital_df[(capital_df.index >= rows_split[2])]
+
+                        capital_movement = _parse_sheet(capital_movement_df, capital_movement_matrix)
+
+                        capital_correctness = _parse_sheet(capital_correctness_df, capital_correctness_matrix)
+                        capital_correctness = capital_correctness.iloc[::-1]
+
+                        row = clean_assets_df[clean_assets_df == '3600'].any(axis=1)
+                        row = row.index[row][0]
+                        clean_assets = clean_assets_df.loc[row, :]
+                        clean_assets = pd.DataFrame(clean_assets[clean_assets.notna()]).reset_index(drop=True).loc[
+                                       2:].reset_index(drop=True)
+                        clean_assets.columns = ['3600']
+                    cash_flow = _parse_sheet(cash_flow_df, cash_flow_matrix)
+
+                    balance.index = list(map(lambda x: year - x, balance.index))
+                    financial_results.index = list(map(lambda x: year - x, financial_results.index))
+                    additional_financial_results.index = list(
+                        map(lambda x: year - x, additional_financial_results.index))
+                    target_usage.index = list(map(lambda x: year - x, target_usage.index))
+                    capital_movement.index = list(map(lambda x: year - x, capital_movement.index))
+                    capital_correctness.index = list(map(lambda x: year - 1 - x, capital_correctness.index))
+                    clean_assets.index = list(map(lambda x: year - x, clean_assets.index))
+                    cash_flow.index = list(map(lambda x: year - x, cash_flow.index))
+
+                    df: pd.DataFrame = pd.concat(
+                        [balance, financial_results, additional_financial_results, target_usage,
                          capital_movement, capital_correctness, clean_assets, cash_flow],
                         axis=1)
-                    inn_chunk.reset_index(names='year', inplace=True)
-                    inn_chunk['inn'] = inn
-                    inn_chunk['reporting_year'] = year
-                    inn_chunk.to_sql(
+                    for col in (balance_columns + financial_results_columns + additional_financial_results_columns + target_usage_columns +
+                                capital_movement_columns + capital_correctness_columns + clean_assets_columns + cash_flow_columns):
+                        if col not in df.columns:
+                            df[col] = None
+                    df = df.groupby(level=0, axis=1).first()
+                    for col in df.select_dtypes(include=['object', 'string']):
+                        df[col] = (
+                            df[col]
+                            .str.replace('(-)2', '', regex=False)
+                            .str.replace('(0)2', '', regex=False)
+                            .str.replace(r'[()\sХ-]', '', regex=True))
+                        df[col] = pd.to_numeric(
+                            df[col],
+                            downcast='float',
+                            errors='ignore')
+                    df.reset_index(names='year', inplace=True)
+                    df['inn'] = inn
+                    df['reporting_year'] = year
+                    df.to_sql(
                         name=table_name,
                         con=self.engine,
                         if_exists='append',
                         index=False)
                     logger.info(f'{inn} success')
                 except Exception as e:
-                    logger.exception(e)
+                    logger.exception(f'inn {inn} failed\n{e}')
