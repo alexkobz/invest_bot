1f8ed21c8c913b07e045df5017fde055aeba30d8
alexkobz <alexanderkobzarrr@gmail.com>
2025-03-11 09:26:37 +0300

    FIX report marts

--- clickhouse/views/v_moex_securities.sql
+++ clickhouse/views/v_moex_securities.sql
@@ -1,13 +1,18 @@
 CREATE VIEW v_moex_securities AS
-SELECT DISTINCT
-    s.inn,
-    s_i.secid,
-    s_i.boardid,
-    argMin(s_i.issuesize, s_i.settledate) OVER (PARTITION BY s_i.secid, s_i.boardid ORDER BY s_i.settledate) AS issuesize,
-    toYear(s_i.settledate) AS year,
-    toYYYYMM(s_i.settledate) AS year_month,
-    yearweek(s_i.settledate) AS year_week,
-    s_i.settledate AS settledate
+SELECT
+    s.inn AS inn,
+    s_i.secid AS secid,
+    s_i.boardid AS boardid,
+    toYear(s_i.settledate) AS "year",
+    s.shortname AS shortname,
+    s.name AS name,
+    s.isin AS isin,
+    s.is_traded AS is_security_traded,
+    argMin(s_i.issuesize, s_i.settledate) AS issuesize
 FROM moex_securities_info AS s_i
 JOIN moex_securities AS s ON s.secid = s_i.secid AND s.boardid = s_i.boardid
 WHERE s_i.issuesize IS NOT NULL OR s_i.issuesize != 0
+GROUP BY s.inn, s_i.secid, s_i.boardid, toYear(s_i.settledate),s.shortname,
+    s.name,
+    s.isin,
+    s.is_traded
