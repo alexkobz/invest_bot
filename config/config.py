b2732b7f03bd2bdddea3df47b61aceae750d922a
Alexander Kobzar <alexanderkobzarrr@gmail.com>
2026-01-04 09:45:38 +0300

    UPD config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,19 @@
-from enum import Enum
+from enum import StrEnum
+from pathlib import Path
 
-
-class Mode(Enum):
+class Mode(StrEnum):
     REPLACE = 'replace'
     APPEND = 'append'
+
+PROJECT_DIR: Path = Path(__file__).parent
+CONFIG_DIR: Path = Path(__file__)
+DAGS_DIR: Path = PROJECT_DIR / 'dags'
+DATA_DIR: Path = PROJECT_DIR / 'data'
+DOCKER_DIR: Path = PROJECT_DIR / 'docker'
+DBT_DIR: Path = PROJECT_DIR / 'dbt'
+DOCS_DIR: Path = PROJECT_DIR / 'docs'
+LOGS_DIR: Path = PROJECT_DIR / 'logs'
+PLUGINS_DIR: Path = PROJECT_DIR / 'plugins'
+SCRIPTS_DIR: Path = PROJECT_DIR / 'scripts'
+SRC_DIR: Path = PROJECT_DIR / 'src'
+TESTS_DIR: Path = PROJECT_DIR / 'tests'
